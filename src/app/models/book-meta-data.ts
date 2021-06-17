0614e54e8a3b365aaef17c370ae06972ddd45aa5
nemodaattila <nemodaattila@gmail.com>
2021-06-17 13:47:49 +0200

    metadata saved

--- src/app/models/book-meta-data.ts
+++ src/app/models/book-meta-data.ts
@@ -1,4 +1,20 @@
 export class BookMetaData {
-type?: string;
-category?: [];
+  private format?:{}[];
+  private language?: [];
+  private mainCategory?:{}[];
+  private subCategory?:{}[];
+  private tag?:{}[];
+  private targetAudience?:{}[];
+  private type?: {}[];
+
+  constructor(data: any) {
+
+    this.format = data.format;
+    this.language = data.language;
+    this.mainCategory = data.mainCategory;
+    this.subCategory = data.subCategory ;
+    this.tag = data.tag;
+    this.targetAudience = data.targetAudience;
+    this.type = data.type;
+  }
 }
--- src/app/services/book-meta-data.service.ts
+++ src/app/services/book-meta-data.service.ts
@@ -2,14 +2,18 @@ import {Injectable, OnInit} from '@angular/core';
 import {ServiceParentService} from "./service-parent.service";
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
+import {BookMetaData} from "../models/book-meta-data";
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookMetaDataService extends ServiceParentService implements OnInit {
 
+  metaData?: BookMetaData;
+
   constructor(private http: HttpClient) {
     super();
+
     this.checkMetaDataInLocalStorage();
   }
 
@@ -18,19 +22,45 @@ export class BookMetaDataService extends ServiceParentService implements OnInit 
   }
 
   private checkMetaDataInLocalStorage() {
-    const metadata = localStorage.getItem('metadata');
-    if (metadata) {
-      console.log('van')
-    } else
-      this.readBookMetaData().subscribe(value => {
-        console.log(value)
+    let date;
 
-      }, error => {
-        console.dir(error)
-      });
+    let metadata = localStorage.getItem('metadata');
+    console.log(metadata)
+    if (metadata) {
+
+      [metadata, date] = JSON.parse(metadata);
+
+      let acttime = parseInt((new Date().getTime() / 1000).toFixed(0))
+      let metaDataTimeStamp = (acttime - date)
+      if (metaDataTimeStamp > 3600000) {
+        this.getMetaDataFromServer()
+      } else
+        this.saveMetaToModel(metadata)
+    } else
+      this.getMetaDataFromServer()
   }
 
-  private readBookMetaData(): Observable<any> {
-    return this.http.get<any>(this._backendUrl + '/metadata');
+  private getMetaDataFromServer() {
+    console.log("metadatarefresh")
+    this.http.get<any>(this._backendUrl + '/metadata').subscribe(data => {
+
+      if ((data.hasOwnProperty('success') && data.success === true)) {
+        this.metaData = data.data;
+        this.saveMetaDataToLocalStorage()
+      }
+    }, error => {
+      alert(error.error.text)
+      console.dir(error.error.text)
+      return;
+    });
+  }
+
+  private saveMetaDataToLocalStorage() {
+    let date = parseInt((new Date().getTime() / 1000).toFixed(0))
+    localStorage.setItem("metadata", JSON.stringify([this.metaData, date]))
+  }
+
+  private saveMetaToModel(model: any) {
+    this.metaData = new BookMetaData(model)
   }
 }
